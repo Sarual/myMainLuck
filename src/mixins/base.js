de0c3d336006b09436b1fc975dc013b619eee51b
lijiang <1406792294@qq.com>
2018-12-21 15:23:27 +0800

    完成单页面tab切换

--- src/mixins/base.js
+++ src/mixins/base.js
@@ -127,16 +127,6 @@ export default class baseMixin extends wepy.mixin {
     wx.switchTab({url: url})
   }
 
-  // 随机色
-  $getRandomColor() {
-    let rgb = []
-    for (let i = 0; i < 3; ++i) {
-      let color = Math.floor(Math.random() * 256).toString(16)
-      color = color.length == 1 ? '0' + color : color
-      rgb.push(color)
-    }
-    return '#' + rgb.join('')
-  }
   // 获取用户信息(改：用户需要要注册)
   $getUserId() {
     return service.isFormal ? wepy.getStorageSync('userId') : wepy.getStorageSync('userIdInfo')

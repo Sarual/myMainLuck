a3b50ed124a94c6bdafba3cd116032feccbbdd02
lijiang <1406792294@qq.com>
2019-01-03 09:39:36 +0800

    城市选择待完善

--- src/assets/utils/constant.js
+++ src/assets/utils/constant.js
@@ -24,3 +24,47 @@ export const SEL_CLASS_CODE = "selClassCode";
  */
 export const LATITUDE = "latitude";
 export const LONGITUDE = "longitude";
+
+export const DEFAULT_CITY = '北京';
+
+// 默认的参数配置
+// export const defualtConfig = {
+//   // 接口地址前缀
+//   // API_URL: HOST_URL+"/ethank-sjz-web/rest",
+//   // 默认分页大小
+//   PAGE_SIZE: 10,
+//   // 没有位置信息/位置被禁用的时候默认地址
+//   DEFAULT_CITY: '北京',
+//   // 默认 request 请求附带的版本信息参数
+//   // VERSION_INFO: {
+//   //   channel: 18,
+//   //   deviceType: '5',
+//   //   tagVersion: '5.0.0',
+//   //   deviceName: phoneInfo
+//   // },
+//   // 验证码倒计时时间
+//   VCODE_TIME: 60,
+//   // 下单页直接注册时的默认密码
+//   DEFAULT_PWD: 123456,
+//   // 支付成功回调地址接口
+//   // NOTIFY_URL: HOST_URL+'ethank-sjz-web/rest/notify/v1.6/weiXinNotifyMiniAPP',
+
+//   // 百度地图转腾讯地图坐标接口
+//   API_MAP_URL: 'https://apis.map.qq.com/ws/coord/v1/translate',
+//   // 腾讯地图KEY，用于转换百度地图坐标到腾讯地图坐标
+//   TENGMAP_KEY: 'ANGBZ-JWYWF-GCPJZ-JYXAY-PXP4S-5VBOP',
+//   // 可以预订的最长日期
+//   MAX_BOOKDAY: 30,
+//   // 可以预定的天数
+//   MAX_BOOKDAY_LENGTH: 7,
+//   // 最多可订房间数量
+//   MAX_ROOM_NUMS: 3,
+//   // 腾讯移动分析应用配置
+//   MTA: {
+//     "appID":"500413713",
+//     "eventID":"500413720",
+//     "statPullDownFresh":true,
+//     "statShareApp":true,
+//     "statReachBottom":true
+//   }
+// };
